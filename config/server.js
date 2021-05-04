67630c3553ea8e5e20bcca23481055120789b738
King <king_lai@outlook.com>
2021-05-05 01:14:51 +0800

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  autoReload: { "enabled": true },
+  autoReload: {
+    enabled: true
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'acd0e1abc7b133f63357c91d9a33b568'),
